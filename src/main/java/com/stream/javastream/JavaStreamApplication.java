679951b43b03507575f0ed65445ebf95c2d7a4fd
bashir <bashirabdi1994@gmail.com>
2023-04-23 22:16:39 +0300

    adding properties file and gitignore

--- src/main/java/com/stream/javastream/JavaStreamApplication.java
+++ src/main/java/com/stream/javastream/JavaStreamApplication.java
@@ -1,11 +1,19 @@
 package com.stream.javastream;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaStreamApplication {
+    @Value("${db.url}")
+    private String databaseUrl;
 
+    @Value("${db.username}")
+    private String databaseUsername;
+
+    @Value("${db.password}")
+    private String databasePassword;
     public static void main(String[] args) {
         SpringApplication.run(JavaStreamApplication.class, args);
     }
